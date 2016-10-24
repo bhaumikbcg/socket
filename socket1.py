910b290f10945d0f2b14800e8dfeeef46763c4e8
Bhaumik Dedhia <bhaumikbcg@yahoo.com>
2016-10-23 21:39:39 -0700

    Add files via upload

--- socket1.py
+++ socket1.py
@@ -1,13 +1,14 @@
 import socket
 
 mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-mysock.connect(('www.py4inf.com', 80))
-mysock.send(b'GET http://www.py4inf.com/code/intro-short.txt HTTP/1.0\n\n')
+mysock.connect(('www.pottermore.com', 80))
+cmd = 'GET http://data.pottermore.com/about/us HTTP/1.0\n\n'.encode()
+mysock.send(cmd)
 
 while True:
     data = mysock.recv(512)
     if ( len(data) < 1 ) :
         break
-    print (data);
+    print (data.decode());
 
 mysock.close()
